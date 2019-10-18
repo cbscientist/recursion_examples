66dfe00b31431864fd1999858bbd62b27eb7d2ea
claire salling <claire@cloudbakers.com>
2019-10-18 14:57:33 -0500

    Fix some minor typos in main program. Add flexible argument for list of replacement characters.

--- demo_recursion.py
+++ demo_recursion.py
@@ -1,17 +1,17 @@
-def demo_recursion(my_string, special_character='%'):
+def show_recursion(my_string, special_character='%', replacement_characters = ['0','1']):
     """
-    This is a simple function intended to demostrate
+    This is a simple function intended to demonstrate
     a recursive algorithm.
     """
 
     if special_character in my_string:
-        show_recursion(my_string.replace(special_character, '1', 1))
-        show_recursion(my_string.replace(special_character, '0', 1))
+        for new_character in replacement_characters:
+            show_recursion(my_string.replace(special_character, new_character, 1))
 
     else:
         print(my_string)
 
 
 if __name__=="__main__":
-    show_recursion('%%whats%%updoc%')
+    show_recursion('%%whatsupdoc%')
     
